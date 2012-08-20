9bde932d822aead49da379afd2cc6b15935cc767
Julio Arias <julio.arias@edify.cr>
2012-08-20 16:15:01 -0600

    Bug fixes with color printing

--- init.rb
+++ init.rb
@@ -28,19 +28,19 @@ end
 opts.parse!
 
 begin
-  p opts
+  puts opts
   exit 1
 end unless options[:repo] || options[:basepkg] || options[:name]
 
 # Replace the project name
-p "[0;32m===> Replacing changeme for project name [#{options[:name]}][m"
+puts "\e[0;32m===> Replacing changeme for project name [#{options[:name]}]\e[m"
 `sed -i .bak -e 's/changeme.root/#{options[:name]}.root/g' src/main/webapp/WEB-INF/web.xml`
 `sed -i .bak -e 's/changeme.log/#{options[:name]}.log/g' src/main/resources/logback.xml`
 `sed -i .bak -e 's/changeme.%i.log.zip/#{options[:name]}.%i.log.zip/g' src/main/resources/logback.xml`
 `sed -i .bak -e 's/file:%s\\/.gradle\\/changeme.properties/file:%s\\/.gradle\\/#{options[:name]}.properties/g' src/main/java/com/edify/config/ApplicationConfig.java`
 
 # Replace the base package
-p "[0;32m===> Replacing com.edify for project base package [#{options[:basepkg]}][m"
+puts "\e[0;32m===> Replacing com.edify for project base package [#{options[:basepkg]}]\e[m"
 `sed -i .bak -e 's/group = "com.edify"/group = "#{options[:basepkg]}"/g' build.gradle`
 `sed -i .bak -e 's/com.edify/#{options[:basepkg]}/g' src/main/java/com/edify/config/ApplicationConfig.java`
 `sed -i .bak -e 's/com.edify/#{options[:basepkg]}/g' src/main/java/com/edify/model/User.java`
@@ -49,7 +49,7 @@ p "[0;32m===> Replacing com.edify for project base package [#{options[:basepkg]
 `sed -i .bak -e 's/com.edify/#{options[:basepkg]}/g' src/main/resources/META-INF/spring/applicationContext-repositories.xml`
 `sed -i .bak -e 's/com.edify/#{options[:basepkg]}/g' src/main/webapp/WEB-INF/web.xml`
 
-p "[0;32m===> Moving everything to the new base package [#{options[:basepkg]}][m"
+puts "\e[0;32m===> Moving everything to the new base package [#{options[:basepkg]}]\e[m"
 project_base_pkg_dir = "src/main/java/#{options[:basepkg].gsub('.', '/')}"
 
 FileUtils.mkdir_p project_base_pkg_dir
@@ -60,11 +60,11 @@ FileUtils.mv "src/main/java/com/edify/web", project_base_pkg_dir
 FileUtils.rm_rf "src/main/java/com/edify"
 
 begin
-  p "[0;32m===> Creating first commit[m"
+  puts "\e[0;32m===> Creating first commit\e[m"
   `git add --all .`
   `git commit -m "#{options[:name]} initial commit"`
-  p "[0;32m===> Changing origin url to [#{options[:repo]}][m"
+  puts "\e[0;32m===> Changing origin url to [#{options[:repo]}]\e[m"
   `git remote set-url origin #{options[:repo]}`
 end unless options[:dry]
 
-p "All done!!\n\n Now just 'git push'"
+puts "\e[0;32m===> All done!! <===\e[m\n\n\e[0;33mNow just 'git push'\e[m"
