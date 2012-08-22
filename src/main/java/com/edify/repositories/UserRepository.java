fef9aa1758626845aa45e8020cd23d58d7d05fc9
Julio Arias <julio.arias@edify.cr>
2012-08-21 22:55:37 -0600

    Add dbSeed task
    Use for data population

---
+++ src/main/java/com/edify/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.edify.repositories;
+
+/**
+ * @author: <a href="https://github.com/jarias">jarias</a>
+ */
+public interface UserRepository {
+}
---
+++ src/main/java/com/edify/web/controllers/UsersController.java
@@ -0,0 +1,7 @@
+package com.edify.web.controllers;
+
+/**
+ * @author: <a href="https://github.com/jarias">jarias</a>
+ */
+public class UsersController {
+}
