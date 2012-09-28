526935696e752188b79a7a9c580434d863043d84
Julio Arias <julio.arias@edify.cr>
2012-09-29 00:17:57 +0200

    Apply a random cache buster in dev mode
    Also improve URL rewrite rules and add one for the translations URL

--- src/main/java/com/edify/web/servlet/EnvironmentInterceptor.java
+++ src/main/java/com/edify/web/servlet/EnvironmentInterceptor.java
@@ -8,31 +8,33 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 /**
  * @author jarias
  * @since 6/2/12 7:10 PM
  */
 public class EnvironmentInterceptor extends HandlerInterceptorAdapter {
-  public static final String ENVIRONMENT_PARAM_NAME = "env";
-  public static final String ASSETS_CACHE_BUSTER_PARAM_NAME = "cacheBuster";
+    public static final String ENVIRONMENT_PARAM_NAME = "env";
+    public static final String ASSETS_CACHE_BUSTER_PARAM_NAME = "cacheBuster";
 
-  //Use for cache busting
-  @Value("${git.sha}")
-  private String gitSha;
+    //Use for cache busting
+    @Value("${git.sha}")
+    private String gitSha;
 
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    WebApplicationContext webApplicationContext = RequestContextUtils.getWebApplicationContext(request);
-    String[] activeProfiles = webApplicationContext.getEnvironment().getActiveProfiles();
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        WebApplicationContext webApplicationContext = RequestContextUtils.getWebApplicationContext(request);
+        String[] activeProfiles = webApplicationContext.getEnvironment().getActiveProfiles();
 
-    if (ArrayUtils.contains(activeProfiles, "production")) {
-      request.setAttribute(ENVIRONMENT_PARAM_NAME, "production");
-      request.setAttribute(ASSETS_CACHE_BUSTER_PARAM_NAME, gitSha);
-    } else {
-      request.setAttribute(ENVIRONMENT_PARAM_NAME, "development");
-      request.getSession().setAttribute(ASSETS_CACHE_BUSTER_PARAM_NAME, "");
+        if (ArrayUtils.contains(activeProfiles, "production")) {
+            request.setAttribute(ENVIRONMENT_PARAM_NAME, "production");
+            request.setAttribute(ASSETS_CACHE_BUSTER_PARAM_NAME, gitSha);
+        } else {
+            request.setAttribute(ENVIRONMENT_PARAM_NAME, "development");
+            //In development set the cache buster to a random UUID to avoid caching of static assets
+            request.getSession().setAttribute(ASSETS_CACHE_BUSTER_PARAM_NAME, UUID.randomUUID());
+        }
+        return true;
     }
-    return true;
-  }
 }
