15872dc2a616f023454ff46aa09db0f6a28aea4b
Julio Arias <julio.arias@edify.cr>
2012-09-30 20:52:58 +0200

    Fix cache buster for JS/CSS for heroku deployments

--- src/main/java/com/edify/web/servlet/EnvironmentInterceptor.java
+++ src/main/java/com/edify/web/servlet/EnvironmentInterceptor.java
@@ -1,6 +1,7 @@
 package com.edify.web.servlet;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,13 +15,24 @@ import java.util.UUID;
  * @author jarias
  * @since 6/2/12 7:10 PM
  */
-public class EnvironmentInterceptor extends HandlerInterceptorAdapter {
+public class EnvironmentInterceptor extends HandlerInterceptorAdapter implements InitializingBean {
     public static final String ENVIRONMENT_PARAM_NAME = "env";
     public static final String ASSETS_CACHE_BUSTER_PARAM_NAME = "cacheBuster";
 
     //Use for cache busting
     @Value("${GIT_SHA}")
     private String gitSha;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        /*
+        Seems like in Heroku there is no way to get the GIT SHA commit
+        So we set it to a random UUID each deploy would be a different value so it works in similar fashion as the git sha
+        */
+        if (gitSha.equals("1")) {
+            gitSha = UUID.randomUUID().toString();
+        }
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
